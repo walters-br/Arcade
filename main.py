81d449b5a96d0b691f347f7f7e0124e3459d50a1
walters-br <ajaywalters195@gmail.com>
2022-12-03 18:55:13 -0500

    Fixed termination Errors. Added A numberguesser.

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ class RPS(Score): # rock paper scissors
                 print("Plater 2 wins, Scissors beats Paper")
 
      Continue = input('Play againz?(y/n):')    # unit test 
-     if Continue == 'y': 
+     if Continue == 'n': 
         break 
      else: 
         continue 
@@ -72,19 +72,19 @@ class HeadsOrTails(Score):  # each player bets on heads or tail
         super().__init__(player1,player2)
         while True: 
           answer = random.randint(1,2)  # turn into a method 
-          player1_guess = int(" Plater 1, State your guess H or T")
-          player2_guess = int(" Plater 1, State your guess H or T")
+          player1_guess = input(" Plater 1, State your guess H or T")
+          player2_guess = input(" Plater 1, State your guess H or T")
           
           if player1_guess == "H": 
-                player1_guess=0 
+                player1_guess=1 
           if player2_guess == "H": 
-                player2_guess=0
+                player2_guess=1
          
 
           if player1_guess == "T": 
-                player1_guess=1 
+                player1_guess=2 
           if player2_guess == "T": 
-                player2_guess=1
+                player2_guess=2
          
           if player1_guess == answer:
                  print('Player 1 guessed right, the answer was: ', answer)
@@ -93,7 +93,9 @@ class HeadsOrTails(Score):  # each player bets on heads or tail
         
           Continue = input('Play againz?(y/n):')    # unit test 
           if Continue == 'y': 
-             break 
+             continue 
+          else:
+            break
 
 
 
@@ -111,7 +113,88 @@ class NumGuesserGame(Score):  # get x amount of guesses, with one player choosin
     
     def __init__(self,player1,player2):
         super().__init__(player1,player2)
-        pass 
+        while True:
+                player1_count=0 
+                player2_count=0
+                number_range= int(input('Player 1, Enter the largest number in your guessing range. You each have 10 chances to guess the number correctly. '))
+                answer = random.randint(1,number_range)  # inputs4
+                
+                number_range2= int(input('Player 2, Enter the largest number in your guessing range. You each have 10 chances to guess the number correctly. '))
+                answer2 = random.randint(1,number_range2)  # inputs4   
+                x=0
+                for x in range(1,12):
+                        player1_guess = int(input("\n Player 1, Take a guess. " ))
+                        x+=1
+                        if player1_guess>answer: 
+                            print("Whoops, you missed it. Your're too high.")
+                            player1_count+=1
+                            print('You have ', 10 - player1_count, ' guesses left.\n')
+                        
+                        elif player1_guess<answer: 
+                            print("\n Your're too low, try guessing higher")
+                            player1_count+=1
+                            print('You have ', 10 - player1_count, ' guesses left.\n')
+                        elif player1_guess == answer: 
+                            print('Congrats you got it! The correct number was ', answer,'.')
+                            break 
+                        
+                        if player1_count==10: 
+                            print('Sorry, you ran out of guesses. The correct number was,',answer,'.')
+                            break
+                    
+            
+                
+                
+                y=0
+                for y in range(1,12):
+                        player2_guess = int(input("\n Player 2, Take a guess. " ))
+                        y+=1
+                        if player2_guess>answer2: 
+                                print("Whoops, you missed it. Your're too high.")
+                                player2_count+=1
+                                print('You have ', 10 - player2_count, ' guesses left.\n')
+                            
+                        elif player2_guess<answer2: 
+                                print("\n Your're too low, try guessing higher")
+                                player2_count+=1
+                                print('You have ', 10 - player2_count, ' guesses left.\n')
+                            
+                        elif player2_guess == answer2: 
+                                print('Congrats you got it! The correct number was ', answer2,'.')
+                                break 
+
+                        if player2_count==10: 
+                                print('Sorry, you ran out of guesses. The correct number was,',answer2,'.')
+                                break
+                Continue = input('Play againz?(y/n):')    # unit test 
+                if Continue == 'y': 
+                 continue 
+                else:
+                    break
+            
+            
+            
+
+
+            
+            
+
+                
+            
+            
+
+
+
+           
+        
+    
+
+
+
+
+
+
+
 
 class TikTakToe(Score): # potential tik tac toe game
      def __init__(self,player1,player2):
